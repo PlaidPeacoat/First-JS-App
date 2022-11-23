a8e587a0b801f4ed222c8e4b2599b3577bc63a1e
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2022-11-23 11:52:17 -0500

    Adding Pokemon
    Added an Array with 3 different pokemon and their data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let favoriteFood = "indian curry"
-document.write(favoriteFood)
+let pokemonList: [
+    { name: 'Pikachu', type: 'electric', XP: 100, height: 1.2 } 
+    { name: 'Bulbasaur', type: 'grass', XP: 50, height: 2.2 } 
+    { name: 'Squirtle', type: 'water', XP: 65, height: 1.8 }
+]
