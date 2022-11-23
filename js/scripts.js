8c8dbaf160ca33bd1c19949d77c75bc4ec723551
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2022-11-23 11:52:52 -0500

    added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//adding an array with three different pokemon and their information 
 let pokemonList: [
     { name: 'Pikachu', type: 'electric', XP: 100, height: 1.2 } 
     { name: 'Bulbasaur', type: 'grass', XP: 50, height: 2.2 } 
