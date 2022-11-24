9f139d4b382382c5ec45ea0ca3581600b81d6d1b
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2022-11-24 16:33:37 -0500

    fixed syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-//adding an array with three different pokemon and their information 
+//adding an array with three different pokemon and their information
 let pokemonList = [
-    { name: 'Pikachu', type: 'electric', XP: 100, height: 1.2 } 
-    { name: 'Bulbasaur', type: 'grass', XP: 50, height: 2.2 } 
-    { name: 'Squirtle', type: 'water', XP: 65, height: 1.8 }
-]
+    { name: "Pikachu", type: "electric", XP: 100, height: 1.2 },
+    { name: "Bulbasaur", type: "grass", XP: 50, height: 2.2 },
+    { name: "Squirtle", type: "water", XP: 65, height: 1.8 },
+];
