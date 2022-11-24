c496d84ee5f3faf4d3961d4988bd20107f4c0b82
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2022-11-24 16:02:24 -0500

    fixed syntax in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //adding an array with three different pokemon and their information 
-let pokemonList: [
+let pokemonList = [
     { name: 'Pikachu', type: 'electric', XP: 100, height: 1.2 } 
     { name: 'Bulbasaur', type: 'grass', XP: 50, height: 2.2 } 
     { name: 'Squirtle', type: 'water', XP: 65, height: 1.8 }
