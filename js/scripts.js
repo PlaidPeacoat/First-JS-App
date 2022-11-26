7b795abfa8643785ef7632057b3a79e416fe962d
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2022-11-26 12:04:04 -0500

    added for loop to print out the pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
 //adding an array with three different pokemon and their information
 let pokemonList = [
-    { name: "Pikachu", type: "electric", XP: 100, height: 1.2 },
-    { name: "Bulbasaur", type: "grass", XP: 50, height: 2.2 },
-    { name: "Squirtle", type: "water", XP: 65, height: 1.8 },
+    {
+        id: 25,
+        name: "Pikachu",
+        type: "electric",
+        XP: 100,
+        height: 1.2
+    },
+    {
+        id: 1,
+        name: "Bulbasaur",
+        type: "grass", XP: 50,
+        height: 2.2
+    },
+    {
+        id: 7,
+        name: "Squirtle",
+        type: "water",
+        XP: 65,
+        height: 1.8
+    },
 ];
+
+//printing all pokemon 
+for (let i = 0;
+    i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 2) {
+        document.write(pokemonList[i].name + " is " + pokemonList[i].height + " feet tall and it is a " + pokemonList[i].type + " type! It's the largest here<br>")
+    }
+    else {
+        document.write(pokemonList[i].name + " is " + pokemonList[i].height + " feet tall and it is a " + pokemonList[i].type + " type!<br>")
+    }
+}
