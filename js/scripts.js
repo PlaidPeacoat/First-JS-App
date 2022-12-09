ddca6ce74fc8827377e00cb5a32686155f7c2d27
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2022-12-08 20:43:10 -0500

    created changes for task 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,77 @@
-//adding an array with three different pokemon and their information
-let pokemonList = [
-    {
-        id: 25,
-        name: "Pikachu",
-        type: "electric",
-        XP: 100,
-        height: 1.2
-    },
-    {
-        id: 1,
-        name: "Bulbasaur",
-        type: "grass", XP: 50,
-        height: 2.2
-    },
-    {
-        id: 7,
-        name: "Squirtle",
-        type: "water",
-        XP: 65,
-        height: 1.8
-    },
-];
+let pokemonRepository = (function () {
 
-//printing all pokemon 
-for (let i = 0;
-    i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 2) {
-        document.write(pokemonList[i].name + " is " + pokemonList[i].height + " feet tall and it is a " + pokemonList[i].type + " type! It's the largest starter Pokemon<br>")
+    let pokemonList = [];
+
+    function add(pokemon) {
+        if (typeof pokemon === 'object' && pokemon.name && pokemon.height && pokemon.type && Object.keys(pokemon).length === 3) {
+            pokemonList.push(pokemon);
+        } else {
+            return `${pokemon} is not a Pokémon. Pokémon must be an object with the keys name, height and type`;
+        }
     }
-    else {
-        document.write(pokemonList[i].name + " is " + pokemonList[i].height + " feet tall and it is a " + pokemonList[i].type + " type!<br>")
+
+    function getAll() {
+        return pokemonList
     }
+
+    return {
+        add: add,
+        getAll: getAll
+
+    };
+
+})();
+
+pokemonRepository.add({
+    id: 25,
+    name: "Pikachu",
+    type: "electric",
+    XP: 100,
+    height: 1.2
+});
+pokemonRepository.add({
+    id: 1,
+    name: "Bulbasaur",
+    type: "grass", XP: 50,
+    height: 2.2
+});
+pokemonRepository.add({
+    id: 7,
+    name: "Squirtle",
+    type: "water",
+    XP: 65,
+    height: 1.8
+});
+pokemonRepository.add({
+    id: 157,
+    name: "Typhlosion",
+    type: "fire",
+    XP: 73,
+    height: 1.7
+});
+
+let pokemonList = pokemonRepository.getAll();
+
+
+//forEach() function to iterate over the Pokémon in the pokemonList array (external function)
+pokemonList.forEach(printDetails);
+function printDetails(pokemon) {
+    let highlight = '';
+    if (pokemon.height > 2.0) {
+        highlight = " - Wow, that’s big!";
+    }
+    document.write(`<li>${pokemon.name} (height: ${pokemon.height}) ${highlight}</li>`);
 }
+
+let pokemonNames = [];
+for (let i = 0; i < pokemonList.length; i++) {
+    pokemonNames.push(pokemonList[i].name);
+}
+
+function findPokemons(query) {
+    return pokemonNames.filter(function (name) {
+        return name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    });
+}
+
+console.log(findPokemons('Iv'))
